90a1c724a2ba411f4eb4615740279961d9a5eca2
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-06-15 23:46:17 +0900

    Updated toXML.vb

--- toXML.vb
+++ toXML.vb
@@ -1,26 +1,44 @@
-
 Sub toXML()
 
-	Application.ScreenUpdating = False
+    Application.ScreenUpdating = False
 
-	Dim sname As String
-	Dim headerRows As Integer
-	Dim lastRowIndex As Integer
-	Dim lastColIndex As Integer
-	Dim ignoreCols As Integer
-	Dim activeRowIndex As Integer
-	Dim activeColIndex As Integer
+    Dim sname As String
+    Dim headerRows As Integer
+    Dim lastRowIndex As Integer
+    Dim lastColIndex As Integer
+    Dim ignoreCols As Integer
+    Dim activeRowIndex As Integer
+    Dim activeColIndex As Integer
+    Dim oneRowCharCount As Integer
+    Dim sBuff() As String
+        Dim ary()
+        Dim cellTo As Object
+        Dim cellFrom As Object
+    Dim i As Integer
 
-	headerRows = 2
-	lastRowIndex = Cells(Rows.Count, 1).End(xlUp).Row
-	lastColIndex = Cells(1, Columns.Count).End(xlToLeft).Column
-	ignoreCols = 3
+    headerRows = 2
+    lastRowIndex = Cells(Rows.Count, 1).End(xlUp).Row
+    lastColIndex = Cells(1, Columns.Count).End(xlToLeft).Column
+    ignoreCols = 3
 
-	For activeRowIndex = (headerRows + 1) To lastRowIndex
-		For activeColIndex = (ignoreCols + 1) To lastColIndex
-			Cells(activeRowIndex, activeColIndex).Value = 1
-		Next activeColIndex
-	Next activeRowIndex
+        ' cellTo = Cells(1, ignoreCols)
+        ' cellFrom = Cells(lastRowIndex, lastColIndex)
+
+        ary = Range(Cells(1, ignoreCols), Cells(lastRowIndex, lastColIndex))
+
+    oneRowCharCount = 0
+    For i = ignoreCols To lastColIndex
+        oneRowCharCount = oneRowCharCount + Len(Cells(1, i))
+    Next i
+
+        ReDim sBuff(1 To (oneRowCharCount * 1.2) * lastRowIndex)
+        MsgBox (oneRowCharCount * 1.2) * lastRowIndex
+
+    For activeRowIndex = (headerRows + 1) To lastRowIndex
+        For activeColIndex = (ignoreCols + 1) To lastColIndex
+            Cells(activeRowIndex, activeColIndex).Value = activeRowIndex * activeColIndex
+        Next activeColIndex
+    Next activeRowIndex
 
 End Sub
 
