895d8288f380ca622b32bda6f7c7093255e8ce18
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-06-16 01:57:15 +0900

    Updated: toXML.vb

--- toXML.vb
+++ toXML.vb
@@ -3,42 +3,55 @@ Sub toXML()
     Application.ScreenUpdating = False
 
     Dim sname As String
-    Dim headerRows As Integer
+    Dim headerTotalRows As Integer
     Dim lastRowIndex As Integer
     Dim lastColIndex As Integer
     Dim ignoreCols As Integer
     Dim activeRowIndex As Integer
     Dim activeColIndex As Integer
     Dim oneRowCharCount As Integer
+    Dim headerRow As Integer
+    Dim table()
     Dim sBuff() As String
-        Dim ary()
-        Dim cellTo As Object
-        Dim cellFrom As Object
+    Dim tmpStr As String
+    Dim targetHeader As String
+    Dim xml As String
     Dim i As Integer
 
-    headerRows = 2
+    headerTotalRows = 2
+    headerRow = 2
     lastRowIndex = Cells(Rows.Count, 1).End(xlUp).Row
     lastColIndex = Cells(1, Columns.Count).End(xlToLeft).Column
     ignoreCols = 3
 
-        ' cellTo = Cells(1, ignoreCols)
-        ' cellFrom = Cells(lastRowIndex, lastColIndex)
+    table = Range(Cells(1, ignoreCols + 1), Cells(lastRowIndex, lastColIndex))
+    ' header: table(headerRow, [数字])
 
-        ary = Range(Cells(1, ignoreCols), Cells(lastRowIndex, lastColIndex))
+    ReDim sBuff(1 To UBound(table, 1) * UBound(table, 2) * 2)
 
-    oneRowCharCount = 0
-    For i = ignoreCols To lastColIndex
-        oneRowCharCount = oneRowCharCount + Len(Cells(1, i))
-    Next i
-
-        ReDim sBuff(1 To (oneRowCharCount * 1.2) * lastRowIndex)
-        MsgBox (oneRowCharCount * 1.2) * lastRowIndex
-
-    For activeRowIndex = (headerRows + 1) To lastRowIndex
+    xml = "<?xml version=""1.0"" encoding=""UTF-8""?>" & vbCrLf
+    xml = xml & "<ExcelField>" & vbCrLf
+    i = 1
+    For activeRowIndex = (headerTotalRows + 1) To lastRowIndex
+        sBuff(i) = "<Data>" & vbCrLf & "<RowNum>" & activeRowIndex - headerTotalRows & "</RowNum>" & vbCrLf
+        i = i + 1
         For activeColIndex = (ignoreCols + 1) To lastColIndex
-            Cells(activeRowIndex, activeColIndex).Value = activeRowIndex * activeColIndex
+          targetHeader = table(headerRow, activeColIndex - ignoreCols)
+          tmpStr = tmpStr & "<" & targetHeader & ">"
+          tmpStr = tmpStr & Cells(activeRowIndex, activeColIndex).Value
+          tmpStr = tmpStr & "</" & targetHeader & ">" & vbCrLf
+          sBuff(i) = tmpStr
+          tmpStr = ""
+          i = i + 1
         Next activeColIndex
+        sBuff(i) = "</Data>" & vbCrLf
+        i = i + 1
     Next activeRowIndex
+    xml = xml & Join(sBuff, "")
+
+
+    xml = xml & "</ExcelField>"
+    Cells(1, 1).Value = xml
 
 End Sub
 
