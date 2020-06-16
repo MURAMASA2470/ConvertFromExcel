9bc3a970cbab0447f43d4369ed76c4690e0d9a54
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-06-16 22:20:33 +0900

    Update: toXML.vb

--- toXML.vb
+++ toXML.vb
@@ -16,44 +16,54 @@ Sub toXML()
     Dim tmpStr As String
     Dim targetHeader As String
     Dim xml As String
+    Dim wrapper As String
+    Dim container As String
+    Dim charCode As String
     Dim i As Integer
 
+    ' ヘッダーの合計業
     headerTotalRows = 2
+    ' ヘッダー名参照行の指定
     headerRow = 2
-    lastRowIndex = Cells(Rows.Count, 1).End(xlUp).Row
-    lastColIndex = Cells(1, Columns.Count).End(xlToLeft).Column
+    ' 左端から無視する列数
     ignoreCols = 3
+    ' ルート要素のタグ名
+    wrapper = "Wrapper"
+    ' 各データを囲むタグ名
+    container = "Container"
+    ' 文字コード
+    charCode = "UTF-8" ' Shift_JIS
+    ' 保存先
+    savePath = Application.ThisWorkbook.Path & "/" & Application.ThisWorkbook.Name & ".xml"
 
     table = Range(Cells(1, ignoreCols + 1), Cells(lastRowIndex, lastColIndex))
     ' header: table(headerRow, [数字])
 
     ReDim sBuff(1 To UBound(table, 1) * UBound(table, 2) * 2)
 
-    xml = "<?xml version=""1.0"" encoding=""UTF-8""?>" & vbCrLf
-    xml = xml & "<ExcelField>" & vbCrLf
+    xml = "<?xml version=""1.0"" encoding=""" & charCode & """?>" & vbCrLf
+    xml = xml & "<" & wrapper & ">" & vbCrLf
     i = 1
     For activeRowIndex = (headerTotalRows + 1) To lastRowIndex
-        sBuff(i) = "<Data>" & vbCrLf & "<RowNum>" & activeRowIndex - headerTotalRows & "</RowNum>" & vbCrLf
+        sBuff(i) = "<" & container & ">" & vbCrLf & vbTab & "<RowNum>" & activeRowIndex - headerTotalRows & "</RowNum>" & vbCrLf
         i = i + 1
         For activeColIndex = (ignoreCols + 1) To lastColIndex
           targetHeader = table(headerRow, activeColIndex - ignoreCols)
-          tmpStr = tmpStr & "<" & targetHeader & ">"
+          tmpStr = tmpStr & vbTab & "<" & targetHeader & ">"
           tmpStr = tmpStr & Cells(activeRowIndex, activeColIndex).Value
           tmpStr = tmpStr & "</" & targetHeader & ">" & vbCrLf
           sBuff(i) = tmpStr
           tmpStr = ""
           i = i + 1
         Next activeColIndex
-        sBuff(i) = "</Data>" & vbCrLf
+        sBuff(i) = "</" & container & ">" & vbCrLf
         i = i + 1
     Next activeRowIndex
     xml = xml & Join(sBuff, "")
-    xml = xml & "</ExcelField>"
+    xml = xml & "</" & wrapper & ">"
 
-    Open Application.ThisWorkbook.Path & "/" & Application.ThisWorkbook.Name & ".xml" For Output As #1
+    Open savePath For Output As #1
       Print #1, xml
     Close #1
 
 End Sub
-
-
